a5222c103c51bd47f77e164cc8f26c039b836b7e
Harrison <harrison@theshoebridges.com>
2014-11-06 13:30:25 +1100

    Basic question and answering system in place

--- main.js
+++ main.js
@@ -1,6 +1,30 @@
-console.log("Hello World")
-
 questions = [{question: "Why is the sky blue", answer: "1"}, {question: "Who was xxx", answer: "2"}, {question:"Why eat cheese", answer:"3"}]
+questionIndex = 0
+$(document).ready(function() {
+	askQuestion()
+	$("#go").click(function() {
+		checkAnswer()
+	})
+
+	function askQuestion() {
+		index = questionIndex
+		while (index == questionIndex) {
+			index = rand(0, questions.length -1)
+			$("#question").text(questions[index].question)
+		}
+		questionIndex = index;
+	}
+
+	function checkAnswer() {
+		question = questions[questionIndex]
+
+		if (question.answer == $("#answer").val()) {
+			askQuestion()
+		}else {
+			console.log(question);
+		}
+	}
+});
 
 /* HELPER FUNCTIONS */
 function rand(min, max) {
