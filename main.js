ab53573c4639a3ac704d107b30cf47022c087a50
Harrison <harrison@theshoebridges.com>
2014-11-06 13:56:33 +1100

    Proper answer selecting, multiple choice system

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-questions = [{question: "Why is the sky blue", answer: "1"}, {question: "Who was xxx", answer: "2"}, {question:"Why eat cheese", answer:"3"}]
+questions = [{question: "Why is the sky blue", answers: [{text:"1", correct: true}, {text: "2", correct: false}]}]
 questionIndex = 0
 $(document).ready(function() {
 	askQuestion()
@@ -8,20 +8,28 @@ $(document).ready(function() {
 
 	function askQuestion() {
 		index = questionIndex
-		while (index == questionIndex) {
-			index = rand(0, questions.length -1)
-			$("#question").text(questions[index].question)
+		while (index == questionIndex && questions.length != 1) {
+			index = rand(0, questions.length)
 		}
 		questionIndex = index;
+		question = questions[questionIndex]
+		$('#question').text(question.question)
+
+		$(".answer").remove()
+		for (var i = 0; i < question.answers.length; i++) {
+			$("<label for='RDIOS' class='answer'>"+question.answers[i].text+"</label><input class='answer' type='radio' name='answers' value='"+question.answers[i].text+"' data-index='"+i+"'/>").appendTo('#answer-container')
+		}
 	}
 
 	function checkAnswer() {
 		question = questions[questionIndex]
-
-		if (question.answer == $("#answer").val()) {
+		// console.log("QUESTION CORRECT", $("input[name='answers']:checked").val(), question);
+		var answer = question.answers[parseInt($("input[name='answers']:checked").attr('data-index'))]
+		console.log(answer)
+		if(answer.correct){
 			askQuestion()
 		}else {
-			console.log(question);
+			console.log("WRONG");
 		}
 	}
 });
